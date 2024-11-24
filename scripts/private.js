5d78c2fdce0aaa6c69942fab28ff33ed34a1d962
Valentin Martinez Reggi <reggivalen@gmail.com>
2024-11-24 17:03:35 -0300

    mod

--- scripts/private.js
+++ scripts/private.js
@@ -21,19 +21,12 @@ document.addEventListener('DOMContentLoaded', async () => {
             throw new Error('Token inválido o expirado');
         }
 
-        // Si la validación es exitosa, muestra el contenido
-        document.getElementById('private-content').style.display = 'block';
+        // Si la validación es exitosa, redirige al panel
+        window.location.href = 'panel.html';
     } catch (error) {
         console.error('Error de autenticación:', error);
         alert('Tu sesión ha expirado. Por favor, inicia sesión nuevamente.');
         localStorage.removeItem('token'); // Limpia el token si es inválido
         window.location.href = 'login.html';
     }
-});
-
-// Lógica para cerrar sesión
-document.getElementById('logout-btn').addEventListener('click', () => {
-    localStorage.removeItem('token'); // Elimina el token del almacenamiento local
-    alert('Has cerrado sesión.');
-    window.location.href = 'login.html'; // Redirige al login
 });
