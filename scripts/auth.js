2cf9f3054d1e6094b1bb38135c905b240ca480db
Valentin Martinez Reggi <reggivalen@gmail.com>
2024-11-24 21:11:39 -0300

    .

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,14 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const token = localStorage.getItem('token'); // Obtiene el token del almacenamiento local
+    const token = localStorage.getItem('token');
 
-    // Si ya hay un token, NO redirigir automáticamente desde index.html
-    if (window.location.pathname.includes('index.html') && token) {
-        console.log('Usuario autenticado, pero no redirigiendo desde index.html.');
-        return;
-    }
-
-    // Si estás en otra página protegida y no tienes token, redirige al login
-    if (!token && (window.location.pathname.includes('panel.html') || window.location.pathname.includes('private.html'))) {
+    if (!token && window.location.pathname.includes('panel.html')) {
         alert('No estás autenticado. Redirigiendo al inicio de sesión.');
         window.location.href = 'login.html';
     }
